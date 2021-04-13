ace93cd2005d70dbfa32d53c2e2613c9dd5e969c
Michael Pingel <mdpingel@gmail.com>
2021-04-12 17:16:06 -0700

    refactor roman numeral function
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>
    Co-authored-by: Louie Schoenknecht <luisesch97@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,35 +1,7 @@
-function convert(number, letter) {
-  let array = [];
-  for (let i = 0; i < number; i++) {
-    array.push(letter);
-  }
-  return array.join('');
-}
-
-function romanNumerals(input) {
-  let sentence = [];
-  let romNum = ['M', 'D', 'C', 'L', 'X', 'V', 'I'];
-  let parameters = [1000, 500, 100, 50, 10, 5, 1];
-
-  for (let i = 0; i < parameters.length; i++) {
-    let num = Math.trunc(input / parameters[i]);
-    input = (input % parameters[i]);
-    let ret = convert(num, romNum[i]);
-    if ((ret.length > 3) && (i !== 0) && (sentence[i - 1] === romNum[i - 1])) {
-      sentence.pop();
-      sentence.push(romNum[i]);
-      sentence.push(romNum[i - 2]);
-    } else if ((ret.length > 3) && (i !== 0)) {
-      sentence.push(romNum[i] + romNum[i - 1]);
-    } else {
-      sentence.push(ret);
-    }
-  }
-
-  return sentence.join('');
-}
-
 function romNumRec(num) {
+  if (isNaN(num) || num < 0) {
+    return 'Invalid input';
+  }
   if (num < 5) {
     return (num === 4) ? "IV" : "I".repeat(num);
   } else if (num < 10) {
@@ -46,33 +18,13 @@ function romNumRec(num) {
     const numOfCs = parseInt(num / 100);
     const updatedNum = num % 100;
     return ((numOfCs === 4) ? "CD" : "C".repeat(numOfCs)) + romNumRec(updatedNum);
+  } else if (num < 1000) {
+    const updatedNum = (num >= 900) ? num - 900 : num - 500;
+    return ((num >= 900) ? "CM" : "D") + romNumRec(updatedNum);
+  } else if (num < 4000) {
+    const numOfMs = parseInt(num / 1000);
+    return "M".repeat(numOfMs) + romNumRec((num % 1000));
+  } else {
+    return "number too thicc";
   }
 }
-
-// please this?
-
-close
-95 = XCXLV
-
-
-
-// function recursionRomanNumeral(num) {
-//   if (isNaN(num)) {
-//     return ` Invalid input `;
-//   }
-//   if (num < 5) {
-//     return "I".repeat(num);
-//   } else if (num < 10) {
-//     const newNum = num % 5;
-//     const nickels = parseInt(cents / 5);
-//     return recursionCoinCounter(newChange) + `| Nickels: ${nickels} |`;
-//   } else if (cents < 25) {
-//     const newChange = cents % 10 / 100;
-//     const dimes = parseInt(cents / 10);
-//     return recursionCoinCounter(newChange) + `| Dimes: ${dimes} |`;
-//   } else {
-//     const newChange = cents % 25 / 100;
-//     const quarters = parseInt(cents / 25);
-//     return recursionCoinCounter(newChange) + `| Quarters: ${quarters} |`;
-//   }
-// }
